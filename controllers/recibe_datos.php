a6c60e06a44457908358650b6189ba5ab3481ffd
Nicolas <nicolascasclew@gmail.com>
2023-10-03 12:04:22 +0100

    cambios eliminados para evitar conflictos

--- controllers/recibe_datos.php
+++ controllers/recibe_datos.php
@@ -1,7 +1,9 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") { //&& $_POST[owner]== "gonzalo"/"nicolas"
     // Obtén el valor del campo "nombre" del formulario
     $nombre = $_POST["nombre"];
+    // $isla = $_POST["isla"];
+
 
     // Realiza alguna acción con los datos, como guardarlos en una base de datos o mostrarlos en la página
     echo "¡Hola, " . $nombre . "!";
