77a4ae3c0c0a787efca7ff77a7cd3b3374d80c61
Nicolas <nicolascasclew@gmail.com>
2023-10-09 08:38:08 +0100

    añadido mas contenido al form, y modificado el if para incluir mi formulario

--- controllers/recibe_datos.php
+++ controllers/recibe_datos.php
@@ -8,4 +8,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" &&  $_POST["formOwner"] == "gonzalo") {
     echo $_POST["formOwner"];
     echo "<br>";
     echo var_dump($_POST);
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST" &&  $_POST["formOwner"] == "nicolas") {
+    $nombre = $_POST["nombre"];
+    $isla = $_POST["isla"];
+    $altura = $_POST["altura"];
+    echo "¡Hola, " . $nombre . "!";
+    echo "<br>";
+    echo "Isla = " . $isla;
+    echo "<br>";
+    echo "Estas a " . $altura . " metros sobre el nivel del mar";
+    echo "<br>";
+
+    echo $_POST["formOwner"];
+    echo "<br>";
 }
--- views/formularioNico.php
+++ views/formularioNico.php
@@ -1,8 +1,9 @@
 <form method="post" action="../controllers/recibe_datos.php">
+    <input type="text" name="formOwner" value="nicolas" hidden>
     <label for="nombre">Nombre:</label>
     <input type="text" name="nombre" id="nombre" placeholder="Nombre">
     </br>
-    <input type="radio" id="gc" name="isla" value="Gran Canaria">
+    <input type="radio" id="gc" name="isla" value="Gran Canaria" checked>
     <label for="gc">Gran Canaria</label><br>
     <input type="radio" id="lz" name="isla" value="Lanzarote">
     <label for="lz">Lanzarote</label><br>
@@ -11,5 +12,18 @@
     <input type="radio" id="lg" name="isla" value="La Gomera">
     <label for="lg">La Gomera</label>
     </br>
+    <input type="range" id="altura" name="altura" min="0" max="150" />
+    <label for="altura">Altura</label>
+    <h5>Numero= <output id="value"></output> </h5>
+    </br>
     <input type="submit" value="Enviar">
+
 </form>
+<script>
+    const value = document.querySelector("#value");
+    const input = document.querySelector("#altura");
+    value.textContent = input.value;
+    input.addEventListener("input", (event) => {
+        value.textContent = event.target.value;
+    });
+</script>
