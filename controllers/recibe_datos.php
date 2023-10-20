f25bdb0423234a09ee65a9aae62386f775f6f119
Nicolas <nicolascasclew@gmail.com>
2023-10-20 12:22:21 +0100

    files now get saved in folder, check repeats TODO

--- controllers/recibe_datos.php
+++ controllers/recibe_datos.php
@@ -17,6 +17,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" &&  $_POST["formOwner"] == "gonzalo") {
     $dinero = $_POST["dinero"];
     $file = $_POST["myfile"];
     print_r($_FILES);
+    $namm = $_FILES["myfile"]["name"];
+    $uploads_dir = '../archivos';
+
+    echo $namm . " es el file";
     if (validateMail($email) && isNumber($dinero) && hasContent($nombre)) {
         echo "¡Hola, " . $nombre . "!";
         echo "<br>";
@@ -24,6 +28,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" &&  $_POST["formOwner"] == "gonzalo") {
         echo $agencia . " se dirige a tu direccion actual en este instante a recuperar sus " . $dinero . "€";
         echo "<br>";
         //phpinfo(-1);
+        $isOk = $_FILES["myfile"]["error"];
+        if ($isOk == 0) {
+            echo "ta rumbando";
+            $tmp_name = $_FILES["myfile"]["tmp_name"];
+            // basename() puede evitar ataques de denegación de sistema de ficheros;
+            // podría ser apropiada más validación/saneamiento del nombre del fichero
+            $name = basename($_FILES["myfile"]["name"]);
+            move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        }
     } else {
         echo "y el file???";
 
--- views/formularioNico.php
+++ views/formularioNico.php
@@ -22,23 +22,29 @@
     <div>
         <input type="text" name="formOwner" value="nicolas" hidden>
         <label for="nombre">Nombre legal:</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Nombre completo" value=<?php echo $default["nombre"] ?>>
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre completo"
+            value=<?php echo $default["nombre"] ?>>
     </div>
     </br>
     <div>
         <label for="correo">Email: </label>
-        <input type="text" name="correo" id="correo" placeholder="Correo electronico" value=<?php echo $default["email"] ?>>
+        <input type="text" name="correo" id="correo" placeholder="Correo electronico"
+            value=<?php echo $default["email"] ?>>
     </div>
     </br>
     <div>
         <h4>A quien le debes dinero?</h4>
-        <input type="radio" id="mh" name="agencia" value="El Ministerio de Hacienda" <?php echo ($default["deudor"]) == "hacienda" ? "checked" : "" ?>>
+        <input type="radio" id="mh" name="agencia" value="El Ministerio de Hacienda"
+            <?php echo ($default["deudor"]) == "hacienda" ? "checked" : "" ?>>
         <label for="mh">Ministerio de Hacienda</label><br>
-        <input type="radio" id="at" name="agencia" value="La Agenda tributaria" <?php echo ($default["deudor"]) == "agencia" ? "checked" : "" ?>>
+        <input type="radio" id="at" name="agencia" value="La Agenda tributaria"
+            <?php echo ($default["deudor"]) == "agencia" ? "checked" : "" ?>>
         <label for="at">Agenda tributaria</label><br>
-        <input type="radio" id="mr" name="agencia" value="La mafia rusa" <?php echo ($default["deudor"]) == "mafia" ? "checked" : "" ?>>
+        <input type="radio" id="mr" name="agencia" value="La mafia rusa"
+            <?php echo ($default["deudor"]) == "mafia" ? "checked" : "" ?>>
         <label for="mr">La mafia rusa</label>
-        <input type="radio" id="dg" name="agencia" value="La Dirección General del Patrimonio del Estado" <?php echo ($default["deudor"]) == "direccion" ? "checked" : "" ?>>
+        <input type="radio" id="dg" name="agencia" value="La Dirección General del Patrimonio del Estado"
+            <?php echo ($default["deudor"]) == "direccion" ? "checked" : "" ?>>
         <label for="dg">Dirección General del Patrimonio del Estado</label>
     </div>
     </br>
@@ -47,17 +53,17 @@
     <h5> <output id="value"> </output>€ </h5>
 
     <label for="myfile">Declaracion de la renta (2021):</label>
-    <input type="file" id="myfile" name="myfile">
+    <input type="file" name="myfile">
     </br></br></br>
 
     <input type="submit" value="Enviar">
 
 </form>
 <script>
-    const value = document.querySelector("#value");
-    const input = document.querySelector("#dinero");
-    value.textContent = input.value;
-    input.addEventListener("input", (event) => {
-        value.textContent = event.target.value;
-    });
+const value = document.querySelector("#value");
+const input = document.querySelector("#dinero");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+});
 </script>
