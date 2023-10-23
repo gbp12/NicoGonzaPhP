015622d858f395379a02dbcf42f87a621c12afd1
gbp12 <gonzabonilla4896@outlook.es>
2023-10-23 21:02:12 +0100

    hotfix

--- helper/guardar_registros.php
+++ helper/guardar_registros.php
@@ -4,11 +4,8 @@ function save_store($data){
     $file = fopen($file_path, "a");
     fwrite($file,"\n" .  "Datos de Formulario:" . "\n"); 
     foreach ($data as $key => $value) {
-        echo "<br>";
-
         $data = $key . ": " . $value . "\n";
         fwrite($file, $data);
-        echo $data;
     }
     fclose($file);
 }
